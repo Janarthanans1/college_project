33b3640e979e7106bb03e5ddaef767bd1356bb75
Janarthanans1 <innotek2024@gmail.com>
2024-11-26 12:21:24 -0800

    delete admin

--- app/admin_dashboard/api/register_user/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-
-
-
-export async function POST(req){
- const {name,password,dept,email} = await req.json()
- return NextResponse.json({message:"Data resived",status:200})   
-}
